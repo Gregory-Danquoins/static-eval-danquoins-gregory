8e0cb80946000e3f50e9e6dcfcf3070e8b3aaa76
Grégory Danquoins <danquoinsgregory@gmail.com>
2025-04-04 00:08:35 +0200

    last commit ? i hope...

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className="bg-primary w-full p-6 md:flex md:text-sm">
-      <div className="md:w-48">
+      <div className="md:w-48 mb-12">
         <p className="text-white text-left ">
           Travaillez plus intelligemment, collaborez plus efficacement. Essayez
           WorkSphere maintenant !
@@ -13,8 +13,8 @@ const Footer = () => {
           Démarrer
         </button>
       </div>
-      <div className="ml-auto -mt-8">
-        <h4 className="uppercase text-teal-400 text-sm mt-8">pages</h4>
+      <div className="ml-auto">
+        <h4 className="uppercase text-teal-400 text-sm mt-8 lg:mt-0">pages</h4>
         <nav className="text-white flex list-none gap-4 md:flex-col md:gap-1">
           <Link href="/">Home</Link>
           <Link href="/tarifs">About us</Link>
@@ -23,7 +23,7 @@ const Footer = () => {
         </nav>
       </div>
 
-      <div className="mt-4 ml-6 md:-mt-8">
+      <div className="mt-4 md:-mt-8 lg:ml-4">
         <h4 className="uppercase text-teal-400 text-sm mt-8">contacts</h4>
         <address className="text-white">
           <a href="mailto:webmaster@example.com">Hello@logoipsum.com</a>
